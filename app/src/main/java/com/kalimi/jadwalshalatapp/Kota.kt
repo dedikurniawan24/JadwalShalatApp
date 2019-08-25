ddc3dd1faaba61db14348d8e7659d2d75e91b979
wintoro13 <wintoro198@gmail.com>
2019-08-25 10:45:23 +0700

    Initial commit

---
+++ app/src/main/java/com/kalimi/jadwalshalatapp/Kota.kt
@@ -0,0 +1,9 @@
+package com.kalimi.jadwalshalatapp
+
+class Kota {
+    var id: Int? = null
+    var nama: String? = null
+    override fun toString(): String {
+        return nama.toString()
+    }
+}
